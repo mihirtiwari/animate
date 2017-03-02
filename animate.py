322a290c27a1c1fc4896570c335c08867bf80d52
Mihir Tiwari <mtiw999@gmail.com>
2017-03-02 01:48:23 -0500

    started npl

--- animate.py
+++ animate.py
@@ -1,2 +1,16 @@
+from textblob import TextBlob
+import graphic as gp
+
+"""object is a dict and phrase is a string"""
+"""
+Animates the phrase
+"""
 def animate(object, phrase):
-    
+    sentence = TextBlob(phrase)
+    pos = sentence.tags()
+
+    for obj, p in zip(pos, object):
+        if p[0] == obj.get_name() and p[1] == 'NN':
+            gp.display(obj)
+        elif 'VB' in p[1]:
+            gp.show_action(obj, str(p[0])
---
+++ graphic.py
@@ -0,0 +1,10 @@
+
+"""
+Displays the object on the screen
+"""
+def display(object):
+
+"""
+Animates the selected object with the given action
+"""
+def show_action(object, action):
