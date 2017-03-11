572d6577ad4d67a0fbdf48416c84ab14adc6ef24
Mihir Tiwari <mtiw999@gmail.com>
2017-03-11 01:00:24 -0500

    able to display phrases with tkinter from another file

--- animate.py
+++ animate.py
@@ -7,11 +7,13 @@ Animates the phrase
 """
 def animate(object, phrase):
     sentence = TextBlob(phrase)
-    pos = sentence.tags()
+    pos = sentence.tags
 
-    for obj, p in zip(pos, object):
-        if p[0] == obj.get_name() and p[1] == 'NN':
-            gp.display(obj)
-        elif 'VB' in p[1]:
-            gp.show_action(obj, str(p[0]))
-        
+    gp.display(object)
+    # for obj, p in zip(pos, object):
+    #     if p[0] == obj.get_name() and p[1] == 'NN':
+    #         gp.display(obj)
+    #     elif 'VB' in p[1]:
+    #         gp.show_action(obj, str(p[0]))
+
+animate("Hello World", "Cat")
--- graphic.py
+++ graphic.py
@@ -1,10 +1,22 @@
+import Tkinter as tk
+from Tkinter import Label
+# from PIL import Image, ImageTk
+
+root = tk.Tk()
+
+def display_obj(root, obj):
+    w = Label(root, text=str(obj))
+    w.pack()
+
+    root.mainloop()
 
 """
 Displays the object on the screen
 """
 def display(object):
+    display_obj(root, object)
 
-"""
-Animates the selected object with the given action
-"""
-def show_action(object, action):
+# """
+# Animates the selected object with the given action
+# """
+# def show_action(object, action):
